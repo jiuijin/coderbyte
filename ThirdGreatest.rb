fdb373a6dd56e556021c488f6b267eec31238c3f
jiuijin <wldmlwls93@naver.com>
2015-07-31 14:39:29 +0900

    Create ThirdGreatest.rb

---
+++ ThirdGreatest.rb
@@ -0,0 +1,12 @@
+def ThirdGreatest(strArr)
+
+  # code goes here
+  sorted_by = strArr.sort_by {|x| x.length }
+  return sorted_by[-3]
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ThirdGreatest(STDIN.gets)           
+
